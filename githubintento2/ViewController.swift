1a9bbe5c7623caaf217d43f5eff5a42a9016603f
Malberth <mariocarreondescartes329@gmail.com>
2023-03-01 11:41:58 -0600

    Initial Commit

---
+++ githubintento2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubintento2
+//
+//  Created by Mario on 01/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
